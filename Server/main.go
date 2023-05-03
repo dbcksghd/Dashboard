728950fa6fb901540318dc21435676e1212f8c33
yoochanhong <chanhongy6@naver.com>
2023-05-03 23:35:50 +0900

    게시글 등록 기능 추가

--- Server/main.go
+++ Server/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	_ "github.com/go-sql-driver/mysql"
-	"log"
+	"github.com/labstack/echo/v4"
+	"io/ioutil"
 	"os"
 )
 
 type Dashboard struct {
-	Id      int    `json:"id"`
 	Title   string `json:"title"`
 	Content string `json:"content"`
 }
@@ -20,9 +21,22 @@ func main() {
 		panic(err)
 	}
 
-	newPost := Dashboard{Title: "what", Content: "뭐라고??"}
-	_, err = db.Exec("INSERT INTO board (title, content) VALUES (?, ?)", newPost.Title, newPost.Content)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	e := echo.New()
+	e.POST("/newpost", func(c echo.Context) error {
+		var jsonData Dashboard
+		requestBody, err := ioutil.ReadAll(c.Request().Body)
+		if err != nil {panic(err)}
+
+		err = json.Unmarshal(requestBody, &jsonData)
+		if err != nil {
+			panic(err)
+		}
+		newPost := Dashboard{Title: jsonData.Title, Content: jsonData.Content}
+		_, err = db.Exec("INSERT INTO board (title, content) VALUES (?, ?)", newPost.Title, newPost.Content)
+		if err != nil {
+			return c.NoContent(500)
+		}
+		return c.NoContent(201)
+	})
+	e.Logger.Fatal(e.Start(":8080"))
 }
