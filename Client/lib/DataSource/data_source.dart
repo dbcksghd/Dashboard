a251dd5e0fae2df8f8488b37a9ae1246b57dcde6
yoochanhong <chanhongy6@naver.com>
2023-05-05 09:17:29 +0900

    게시글 삭제 기능 추가

--- Client/lib/DataSource/data_source.dart
+++ Client/lib/DataSource/data_source.dart
@@ -20,4 +20,9 @@ class PostDataSource {
       throw Exception("게시글 불러오기 실패");
     }
   }
+
+  Future<void> deletePost(Post post) async {
+    final response = await http.delete(Uri.parse("$postUrl/id?${post.id}"));
+    if (response.statusCode == 500) throw Exception("게시글 삭제 실패");
+  }
 }
