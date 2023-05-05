77839bea58dadad6bacee3a578338aa5bf893f2f
yoochanhong <chanhongy6@naver.com>
2023-05-05 09:53:29 +0900

    게시글 페이지 뷰모델에 context 물려주기

--- Client/lib/View/Screen/post_page.dart
+++ Client/lib/View/Screen/post_page.dart
@@ -1,10 +1,14 @@
+import 'package:client/ViewModel/post_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class PostPage extends StatelessWidget {
-  const PostPage({Key? key}) : super(key: key);
+  PostPage({Key? key}) : super(key: key);
+  late PostViewModel viewModel;
 
   @override
   Widget build(BuildContext context) {
+    viewModel = Provider.of<PostViewModel>(context);
     return Scaffold();
   }
 }
