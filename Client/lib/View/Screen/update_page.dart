be0b9b80679fea0282993ecd08647f2e768eff05
yoochanhong <chanhongy6@naver.com>
2023-05-05 19:56:28 +0900

    수정 페이지 생성

---
+++ Client/lib/View/Screen/update_page.dart
@@ -0,0 +1,41 @@
+import 'package:client/ViewModel/post_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class UpdatePage extends StatelessWidget {
+  UpdatePage({
+    Key? key,
+    required this.id,
+    required this.title,
+    required this.content,
+  }) : super(key: key);
+
+  int id;
+  String title, content;
+  late PostViewModel viewModel;
+
+  @override
+  Widget build(BuildContext context) {
+    viewModel = Provider.of<PostViewModel>(context);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              viewModel.updatePost(id, title, content);
+              Navigator.of(context).pop();
+            },
+            child: const Text("수정", style: TextStyle(color: Colors.black)),
+          ),
+        ],
+      ),
+    );
+  }
+}
