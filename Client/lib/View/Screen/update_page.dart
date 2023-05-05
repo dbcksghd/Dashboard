2a3f60492c564dcdb2faedf148921d5d5b6e5f3a
yoochanhong <chanhongy6@naver.com>
2023-05-05 21:52:35 +0900

    수정 페이지도 똑같이 구현

--- Client/lib/View/Screen/update_page.dart
+++ Client/lib/View/Screen/update_page.dart
@@ -3,7 +3,7 @@ import 'package:client/ViewModel/post_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class UpdatePage extends StatelessWidget {
+class UpdatePage extends StatefulWidget {
   UpdatePage({
     Key? key,
     required this.id,
@@ -13,14 +13,40 @@ class UpdatePage extends StatelessWidget {
 
   int id;
   String title, content;
+
+  @override
+  State<UpdatePage> createState() => _UpdatePageState();
+}
+
+class _UpdatePageState extends State<UpdatePage> {
   late PostViewModel viewModel;
+
   late TextEditingController titleController, contentController;
+  late FocusNode focusNode;
+  final titleFormKey = GlobalKey<FormState>();
+  final contentFormKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    focusNode = FocusNode();
+    titleController = TextEditingController();
+    contentController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    focusNode.dispose();
+    titleController.dispose();
+    contentController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     viewModel = Provider.of<PostViewModel>(context);
-    titleController = TextEditingController(text: title);
-    contentController = TextEditingController(text: content);
+    titleController = TextEditingController(text: widget.title);
+    contentController = TextEditingController(text: widget.content);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -35,11 +61,14 @@ class UpdatePage extends StatelessWidget {
         actions: [
           TextButton(
             onPressed: () {
-              viewModel.updatePost(
-                  id, titleController.text, contentController.text);
-              Navigator.of(context).pushAndRemoveUntil(
-                  MaterialPageRoute(builder: (context) => PostPage()),
-                  (route) => false);
+              if (titleFormKey.currentState!.validate() &&
+                  contentFormKey.currentState!.validate()) {
+                viewModel.updatePost(
+                    widget.id, titleController.text, contentController.text);
+                Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(builder: (context) => PostPage()),
+                        (route) => false);
+              }
             },
             child: const Text("수정", style: TextStyle(color: Colors.black)),
           ),
@@ -60,14 +89,19 @@ class UpdatePage extends StatelessWidget {
                 padding: EdgeInsets.only(
                     left: MediaQuery.of(context).size.width * 0.03,
                     right: MediaQuery.of(context).size.width * 0.03),
-                child: TextField(
-                  maxLines: 1,
-                  style: TextStyle(
-                      fontSize: MediaQuery.of(context).size.width * 0.05),
-                  keyboardType: TextInputType.text,
-                  controller: titleController,
-                  decoration: const InputDecoration(
-                    border: InputBorder.none,
+                child: Form(
+                  key: titleFormKey,
+                  child: TextFormField(
+                    validator: (value) => value!.isEmpty ? '제목을 입력해주세요.' : null,
+                    maxLines: 1,
+                    style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.width * 0.05),
+                    keyboardType: TextInputType.text,
+                    controller: titleController,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                    ),
+                    onFieldSubmitted: (value) => focusNode.requestFocus(),
                   ),
                 ),
               ),
@@ -82,14 +116,20 @@ class UpdatePage extends StatelessWidget {
                 padding: EdgeInsets.only(
                     left: MediaQuery.of(context).size.width * 0.03,
                     right: MediaQuery.of(context).size.width * 0.03),
-                child: TextField(
-                  maxLines: null,
-                  style: TextStyle(
-                      fontSize: MediaQuery.of(context).size.width * 0.05),
-                  keyboardType: TextInputType.multiline,
-                  controller: contentController,
-                  decoration: const InputDecoration(
-                    border: InputBorder.none,
+                child: Form(
+                  key: contentFormKey,
+                  child: TextFormField(
+                    validator: (value) =>
+                        value!.isEmpty ? '게시글을 입력해주세요.' : null,
+                    style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.width * 0.05),
+                    maxLines: null,
+                    focusNode: focusNode,
+                    keyboardType: TextInputType.multiline,
+                    controller: contentController,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                    ),
                   ),
                 ),
               ),
