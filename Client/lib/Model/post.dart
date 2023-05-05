4b7cff48ff1725acd90c56fe1443051c00f4042d
yoochanhong <chanhongy6@naver.com>
2023-05-05 09:01:18 +0900

    게시글 모델 정의

---
+++ Client/lib/Model/post.dart
@@ -0,0 +1,15 @@
+class Post {
+  int? id;
+  String? title;
+  String? content;
+
+  Post({this.id, this.title, this.content});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      id: json['id'],
+      title: json['title'],
+      content: json['content'],
+    );
+  }
+}
