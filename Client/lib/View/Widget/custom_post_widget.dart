f3c5387ce4d2f01185be2f9bc54405a22e86b342
yoochanhong <chanhongy6@naver.com>
2023-05-05 16:59:44 +0900

    게시글 페이지로 네비게이터 달아주기

--- Client/lib/View/Widget/custom_post_widget.dart
+++ Client/lib/View/Widget/custom_post_widget.dart
@@ -1,35 +1,44 @@
 import 'dart:ui';
 
+import 'package:client/View/Screen/content_page.dart';
 import 'package:flutter/material.dart';
 
 class CustomPostWidget extends StatelessWidget {
-  CustomPostWidget({Key? key, required this.title}) : super(key: key);
-  String title;
+  CustomPostWidget({Key? key, required this.title, required this.content})
+      : super(key: key);
+  String title, content;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15.0),
-        border: Border.all(width: 1),
+    return GestureDetector(
+      onTap: () => Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => ContentPage(content: content),
+        ),
       ),
-      width: MediaQuery.of(context).size.width - 20,
-      height: MediaQuery.of(context).size.height - 600,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 15.0),
-            child: Text(
-              title,
-              style: const TextStyle(
-                fontSize: 16.0,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15.0),
+          border: Border.all(width: 1),
+        ),
+        width: MediaQuery.of(context).size.width - 20,
+        height: MediaQuery.of(context).size.height - 600,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 16.0,
+                ),
               ),
             ),
-          ),
-          const IconButton(onPressed: null, icon: Icon(Icons.more_vert))
-        ],
+            const IconButton(onPressed: null, icon: Icon(Icons.more_vert))
+          ],
+        ),
       ),
     );
   }
