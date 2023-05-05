57e627f601a2f20ad8878a4aff82925b7951b698
yoochanhong <chanhongy6@naver.com>
2023-05-05 12:26:45 +0900

    createPost 메소드에 헤더 추가

--- Client/lib/DataSource/data_source.dart
+++ Client/lib/DataSource/data_source.dart
@@ -7,9 +7,12 @@ import 'package:client/Model/post.dart';
 class PostDataSource {
   final String postUrl = 'http://localhost:8080/post';
 
-  Future<void> createPost(Post post) async {
-    final response = await http.post(Uri.parse(postUrl),
-        body: {"title": post.title, "content": post.content});
+  Future<void> createPost(String title, String content) async {
+    final response = await http.post(
+      Uri.parse(postUrl),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'title': title, 'content': content}),
+    );
     if (response.statusCode == 500) {
       throw Exception("게시글 동록 실패");
     }
