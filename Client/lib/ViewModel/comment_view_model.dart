af050d8eb93603db38bb6e87a907fd753c101bae
yoochanhong <chanhongy6@naver.com>
2023-05-06 17:12:35 +0900

    댓글용 뷰모델 파일

---
+++ Client/lib/ViewModel/comment_view_model.dart
@@ -0,0 +1,19 @@
+import 'package:client/Model/comment.dart';
+import 'package:client/Repository/comment_repository.dart';
+import 'package:flutter/material.dart';
+
+class CommentViewModel with ChangeNotifier {
+  late final CommentRepository _repository;
+
+  List<Comment> _postList = List.empty(growable: true);
+
+  List<Comment> get postList => _postList;
+
+  CommentViewModel() {
+    _repository = CommentRepository();
+  }
+
+  Future<void> createPost(int id, String comment) async {
+    await _repository.createPost(id, comment);
+  }
+}
