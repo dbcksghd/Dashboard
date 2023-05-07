c466482bf9ade4b8e477dcc76e91cd7f6f96c922
yoochanhong <chanhongy6@naver.com>
2023-05-07 14:58:01 +0900

    postId 추가

--- Client/lib/Model/comment.dart
+++ Client/lib/Model/comment.dart
@@ -1,13 +1,15 @@
 class Comment {
   int? id;
+  int? postId;
   String? writeTime;
   String? comment;
 
-  Comment({this.id, this.writeTime, this.comment});
+  Comment({this.id, this.postId, this.writeTime, this.comment});
 
   factory Comment.fromJson(Map<String, dynamic> json) {
     return Comment(
       id: json['id'],
+      postId: json['postId'],
       writeTime: json['writeTime'],
       comment: json['comment'],
     );
