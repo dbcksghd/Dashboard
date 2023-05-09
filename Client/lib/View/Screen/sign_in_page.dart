e8635017e06904b2b0080e7638ec021c8620daa9
yoochanhong <chanhongy6@naver.com>
2023-05-09 17:29:49 +0900

    textField 위젯 생성

--- Client/lib/View/Screen/sign_in_page.dart
+++ Client/lib/View/Screen/sign_in_page.dart
@@ -5,6 +5,62 @@ class SignInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[100],
+                borderRadius: const BorderRadius.all(Radius.circular(8)),
+                border: Border.all(width: 1, color: Colors.black12),
+              ),
+              child: Padding(
+                padding: EdgeInsets.only(
+                    left: MediaQuery.of(context).size.width * 0.04,
+                    right: MediaQuery.of(context).size.width * 0.04),
+                child: TextFormField(
+                  validator: (value) => value!.isEmpty ? '아이디를 입력해주세요.' : null,
+                  cursorColor: Colors.black,
+                  style: TextStyle(
+                      height: 1.2,
+                      fontSize: MediaQuery.of(context).size.width * 0.05),
+                  keyboardType: TextInputType.multiline,
+                  decoration: const InputDecoration(
+                    hintText: "아이디를 입력해주세요",
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[100],
+                borderRadius: const BorderRadius.all(Radius.circular(8)),
+                border: Border.all(width: 1, color: Colors.black12),
+              ),
+              child: Padding(
+                padding: EdgeInsets.only(
+                    left: MediaQuery.of(context).size.width * 0.04,
+                    right: MediaQuery.of(context).size.width * 0.04),
+                child: TextFormField(
+                  validator: (value) => value!.isEmpty ? '비밀번호를 입력해주세요.' : null,
+                  cursorColor: Colors.black,
+                  style: TextStyle(
+                      height: 1.2,
+                      fontSize: MediaQuery.of(context).size.width * 0.05),
+                  keyboardType: TextInputType.multiline,
+                  decoration: const InputDecoration(
+                    hintText: "비밀번호를 입력해주세요",
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
