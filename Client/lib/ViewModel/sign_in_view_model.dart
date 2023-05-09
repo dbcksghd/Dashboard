63138ae76b0e9b894bfb7f691e3b00ed3ae63e01
yoochanhong <chanhongy6@naver.com>
2023-05-09 17:15:42 +0900

    로그인 뷰모델 클래스 생성

---
+++ Client/lib/ViewModel/sign_in_view_model.dart
@@ -0,0 +1,24 @@
+import 'package:client/Repository/sign_in_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:client/Model/sign_in_response.dart';
+
+class SignInViewModel with ChangeNotifier {
+  late final SignInRepository _repository;
+
+  SignInResponse? _response = SignInResponse();
+  bool _success = false;
+
+  bool get success => _success;
+
+  SignInViewModel() {
+    _repository = SignInRepository();
+  }
+
+  Future<void> signIn(String id, password, name) async {
+    _response = await _repository.signIn(id, password, name);
+    if (_response != null) {
+      _success = true;
+    }
+    notifyListeners();
+  }
+}
