6b28fc76335ff42b1b2e457942f22d2e43455733
yoochanhong <chanhongy6@naver.com>
2023-05-29 14:28:31 +0900

    피드 레포지토리 생성

---
+++ Server/domain/feed/repository/repository.go
@@ -0,0 +1,10 @@
+package repository
+
+import "Server/domain/feed/entity"
+
+type FeedRepository interface {
+	CreateFeed(feed *entity.Feed) error
+	FindAllFeeds([]*entity.Feed) error
+	DeleteFeed(id int) error
+	UpdateFeed(feed *entity.Feed) error
+}
