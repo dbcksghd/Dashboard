f2ed62aea5b2d729a7e8d54644a1cbfff84427bc
yoochanhong <chanhongy6@naver.com>
2023-05-29 21:38:44 +0900

    FindAllFeeds 추상 메소드 형식 수정

--- Server/domain/feed/repository/repository.go
+++ Server/domain/feed/repository/repository.go
@@ -4,7 +4,7 @@ import "Server/domain/feed/entity"
 
 type FeedRepository interface {
 	CreateFeed(feed *entity.Feed) error
-	FindAllFeeds([]*entity.Feed) error
+	FindAllFeeds() ([]*entity.Feed, error)
 	DeleteFeed(id int) error
 	UpdateFeed(feed *entity.Feed) error
 }
