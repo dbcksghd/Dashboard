1774fba78f3a9feb92279c943e9e714f8127e2ca
yoochanhong <chanhongy6@naver.com>
2023-05-29 22:42:19 +0900

    dto 분리

--- Server/domain/feed/presentation/dto/requset/request.go
+++ Server/domain/feed/presentation/dto/requset/request.go
@@ -1,7 +1,12 @@
 package requset
 
-type Request struct {
+type UpdateRequest struct {
 	Id      int    `json:"id"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
+
+type CreateRequest struct {
 	Title   string `json:"title"`
 	Content string `json:"content"`
 }
