a1b61e49f1da84d27b4ba50f3b43b449dc107367
yoochanhong <chanhongy6@naver.com>
2023-05-29 14:21:49 +0900

    생성, 수정 기능 추가

--- Server/domain/feed/entity/feed.go
+++ Server/domain/feed/entity/feed.go
@@ -5,3 +5,18 @@ type Feed struct {
 	Title   string `json:"title"`
 	Content string `json:"content"`
 }
+
+func CreateFeed(title, content string) *Feed {
+	return &Feed{
+		Title:   title,
+		Content: content,
+	}
+}
+
+func UpdateFeed(id int, title, content string) *Feed {
+	return &Feed{
+		Id:      id,
+		Title:   title,
+		Content: content,
+	}
+}
