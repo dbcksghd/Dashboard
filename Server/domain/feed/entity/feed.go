83c7bf1c8bab07b21246dd0fb8042d083e2d5d3b
yoochanhong <chanhongy6@naver.com>
2023-05-29 20:52:09 +0900

    생성자 패턴

--- Server/domain/feed/entity/feed.go
+++ Server/domain/feed/entity/feed.go
@@ -6,6 +6,13 @@ type Feed struct {
 	Content string `json:"content"`
 }
 
+func NewFeed(title, content string) *Feed {
+	return &Feed{
+		Title:   title,
+		Content: content,
+	}
+}
+
 func (f *Feed) Setter(feed *Feed) {
 	f.Title = feed.Title
 	f.Content = feed.Content
