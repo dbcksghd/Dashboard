51f2b64a6a1c1999fb021677e4457c923c2841eb
yoochanhong <chanhongy6@naver.com>
2023-05-29 21:27:45 +0900

    add UpdateFeed to controller

--- Server/domain/feed/presentation/feedController.go
+++ Server/domain/feed/presentation/feedController.go
@@ -27,3 +27,15 @@ func (h *FeedHandler) CreateFeed(c echo.Context) error {
 	}
 	return c.NoContent(201)
 }
+
+func (h *FeedHandler) UpdateFeed(c echo.Context) error {
+	req := new(requset.Request)
+	if err := c.Bind(req); err != nil {
+		return c.JSON(500, map[string]string{"error": err.Error()})
+	}
+	err := h.feedService.UpdateFeed(req.Id, req.Title, req.Content)
+	if err != nil {
+		return c.JSON(500, map[string]string{"error": err.Error()})
+	}
+	return c.NoContent(201)
+}
