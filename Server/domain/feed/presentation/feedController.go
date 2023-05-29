724beff35c2520864ac5659dd849d4fec9872760
yoochanhong <chanhongy6@naver.com>
2023-05-29 21:22:43 +0900

    createfeed controller 생성
    프레젠테이션

---
+++ Server/domain/feed/presentation/feedController.go
@@ -0,0 +1,29 @@
+package presentation
+
+import (
+	"Server/domain/feed/presentation/dto/requset"
+	"Server/domain/feed/service"
+	"github.com/labstack/echo/v4"
+)
+
+type FeedHandler struct {
+	feedService service.FeedService
+}
+
+func NewFeedHandler(feedService service.FeedService) *FeedHandler {
+	return &FeedHandler{
+		feedService: feedService,
+	}
+}
+
+func (h *FeedHandler) CreateFeed(c echo.Context) error {
+	req := new(requset.Request)
+	if err := c.Bind(req); err != nil {
+		return c.JSON(500, map[string]string{"error": err.Error()})
+	}
+	err := h.feedService.CreateFeed(req.Title, req.Content)
+	if err != nil {
+		return c.JSON(500, map[string]string{"error": err.Error()})
+	}
+	return c.NoContent(201)
+}
