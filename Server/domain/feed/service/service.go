6a2b87a71cadd29530cee0e8fb74bb65e484b3f7
yoochanhong <chanhongy6@naver.com>
2023-05-30 13:57:02 +0900

    entity 사용

--- Server/domain/feed/service/service.go
+++ Server/domain/feed/service/service.go
@@ -7,31 +7,25 @@ import (
 )
 
 type FeedService struct {
-	feedRepository repository.FeedRepository
+	repositoryImpl repository.FeedRepositoryImpl
 }
 
-func (f *FeedService) CreateFeed(title, content string) error {
-	feed := entity.NewFeed(title, content)
-	if err := f.feedRepository.CreateFeed(feed); err != nil {
+func (f *FeedService) CreateFeed(feed *entity.Feed) error {
+	if err := f.repositoryImpl.CrateFeed(feed); err != nil {
 		return errors.New("게시글 생성 실패")
 	}
 	return nil
 }
 
-func (f *FeedService) UpdateFeed(id int, title, content string) error {
-	feed := &entity.Feed{
-		Id:      id,
-		Title:   title,
-		Content: content,
-	}
-	if err := f.feedRepository.UpdateFeed(feed); err != nil {
+func (f *FeedService) UpdateFeed(feed *entity.Feed, id int) error {
+	if err := f.repositoryImpl.UpdateFeed(feed, id); err != nil {
 		return errors.New("게시글 수정 실패")
 	}
 	return nil
 }
 
 func (f *FeedService) DeleteFeed(feed *entity.Feed) error {
-	if err := f.feedRepository.UpdateFeed(feed); err != nil {
+	if err := f.repositoryImpl.DeleteFeed(feed); err != nil {
 		return errors.New("게시글 삭제 실패")
 	}
 	return nil
