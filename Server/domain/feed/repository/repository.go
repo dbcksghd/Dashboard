f1cfbd8c3e5a523c3f6812c23eb681127e0d3b23
yoochanhong <chanhongy6@naver.com>
2023-05-30 14:04:38 +0900

    db 매개변수 삭제

--- Server/domain/feed/repository/repository.go
+++ Server/domain/feed/repository/repository.go
@@ -3,11 +3,51 @@ package repository
 import (
 	"Server/domain/feed/entity"
 	"Server/domain/feed/presentation/dto/response"
+	"gorm.io/gorm"
 )
 
-type FeedRepository interface {
-	CreateFeed(feed *entity.Feed) error
-	FindAllFeeds() (response.FeedResponse, error)
-	DeleteFeed(id int) error
-	UpdateFeed(feed *entity.Feed) error
+type BoardRepository struct {
+	db *gorm.DB
+}
+
+func (r *BoardRepository) CrateFeed(feed *entity.Feed) error {
+	result := r.db.Table("feed").Create(feed)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (r *BoardRepository) FindAllFeeds() (response.FeedResponse, error) {
+	var res response.FeedResponse
+	result := r.db.Table("feed").Find(&res)
+	if result.Error != nil {
+		return res, result.Error
+	}
+	return res, nil
+}
+
+func (r *BoardRepository) UpdateFeed(feed *entity.Feed, id int) error {
+	result := r.db.Table("feed").Find(feed, "id = ?", id)
+	if result.Error != nil {
+		return result.Error
+	}
+	result = r.db.Table("feed").Save(feed)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (r *BoardRepository) DeleteFeed(id int) error {
+	feed := entity.Feed{}
+	result := r.db.Table("feed").Find(&feed, "id = ?", id)
+	if result.Error != nil {
+		return result.Error
+	}
+	result = r.db.Table("feed").Delete(&feed)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
 }
