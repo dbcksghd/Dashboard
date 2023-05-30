a06cde9b40abdcda8ca75b45cc0958d1d46c8646
yoochanhong <chanhongy6@naver.com>
2023-05-30 13:54:55 +0900

    dto => entity

--- Server/domain/feed/repository/repository.go
+++ Server/domain/feed/repository/repository.go
@@ -1,13 +1,13 @@
 package repository
 
 import (
-	"Server/domain/feed/presentation/dto/requset"
+	"Server/domain/feed/entity"
 	"Server/domain/feed/presentation/dto/response"
 )
 
 type FeedRepository interface {
-	CreateFeed(feed requset.CreateRequest) error
+	CreateFeed(feed *entity.Feed) error
 	FindAllFeeds() (response.FeedResponse, error)
 	DeleteFeed(id int) error
-	UpdateFeed(feed *requset.UpdateRequest) error
+	UpdateFeed(feed *entity.Feed) error
 }
