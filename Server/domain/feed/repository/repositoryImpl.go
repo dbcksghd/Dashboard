aec8540145c6fb0659c3bf384223110f55d0fa20
yoochanhong <chanhongy6@naver.com>
2023-05-30 10:21:35 +0900

    피드 삭제하기 메소드 구현

--- Server/domain/feed/repository/repositoryImpl.go
+++ Server/domain/feed/repository/repositoryImpl.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"Server/domain/feed/entity"
 	"Server/domain/feed/presentation/dto/requset"
 	"Server/domain/feed/presentation/dto/response"
 	"gorm.io/gorm"
@@ -31,6 +32,19 @@ func (receiver FeedRepositoryImpl) UpdateFeed(feed *requset.UpdateRequest, db *g
 		return result.Error
 	}
 	result = db.Table("feed").Save(feed)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (receiver FeedRepositoryImpl) DeleteFeed(id int, db *gorm.DB) error {
+	feed := entity.Feed{}
+	result := db.Table("feed").Find(&feed, "id = ?", id)
+	if result.Error != nil {
+		return result.Error
+	}
+	result = db.Table("feed").Delete(&feed)
 	if result.Error != nil {
 		return result.Error
 	}
