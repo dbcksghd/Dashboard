dffb3ae715bd48737a75641ef15ca25631b67a50
yoochanhong <chanhongy6@naver.com>
2023-05-31 00:40:15 +0900

    핸들러 구현

--- Server/domain/feed/presentation/feedHandler.go
+++ Server/domain/feed/presentation/feedHandler.go
@@ -1,1 +1,20 @@
 package presentation
+
+import "github.com/labstack/echo/v4"
+
+type FeedHandler struct {
+	feedController FeedController
+}
+
+func NewFeedHandler(feedController FeedController) *FeedHandler {
+	return &FeedHandler{
+		feedController: feedController,
+	}
+}
+
+func (f *FeedHandler) RegisterRoutes(e *echo.Echo) {
+	e.POST("/feed", f.feedController.CreateFeed)
+	e.GET("/feed", f.feedController.FindAllFeeds)
+	e.PATCH("/feed", f.feedController.UpdateFeed)
+	e.DELETE("/feed", f.feedController.DeleteFeed)
+}
