b658338bcec028a2e517adc5dc8febaf1d6018ce
yoochanhong <chanhongy6@naver.com>
2023-05-31 10:18:19 +0900

    댓글 레포지토리

---
+++ Server/domain/comment/repository/commentRepository.go
@@ -0,0 +1,33 @@
+package repository
+
+import (
+	"Server/domain/comment/entity"
+	"gorm.io/gorm"
+)
+
+type CommentRepository struct {
+	db *gorm.DB
+}
+
+func NewCommentRepository(db *gorm.DB) *CommentRepository {
+	return &CommentRepository{
+		db: db,
+	}
+}
+
+func (r *CommentRepository) CreateComment(comment *entity.Comment) error {
+	result := r.db.Table("comment").Create(comment)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (r *CommentRepository) FindAllCommentInFeed(id int) (*[]entity.Comment, error) {
+	res := []entity.Comment{}
+	result := r.db.Table("comment").Find(&res, "id = ?", id)
+	if result.Error != nil {
+		return &res, result.Error
+	}
+	return &res, nil
+}
