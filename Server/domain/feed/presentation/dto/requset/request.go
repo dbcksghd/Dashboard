e4d3748a6110ebda625239e5b4ac0c70dcfe1d23
yoochanhong <chanhongy6@naver.com>
2023-07-14 10:31:05 +0900

    feed dto 캡슐화

--- Server/domain/feed/presentation/dto/requset/request.go
+++ Server/domain/feed/presentation/dto/requset/request.go
@@ -1,12 +1,40 @@
 package requset
 
 type UpdateRequest struct {
-	Id      int    `json:"id"`
-	Title   string `json:"title"`
-	Content string `json:"content"`
+	id      int    `gorm:"column:id"`
+	title   string `gorm:"column:title"`
+	content string `gorm:"column:content"`
+}
+
+func (u *UpdateRequest) Id() int {
+	return u.id
+}
+
+func (u *UpdateRequest) Title() string {
+	return u.title
+}
+
+func (u *UpdateRequest) Content() string {
+	return u.content
+}
+
+func NewUpdateRequest(id int, title string, content string) *UpdateRequest {
+	return &UpdateRequest{id: id, title: title, content: content}
 }
 
 type CreateRequest struct {
-	Title   string `json:"title"`
-	Content string `json:"content"`
+	title   string `gorm:"column:title"`
+	content string `gorm:"column:content"`
+}
+
+func (c *CreateRequest) Title() string {
+	return c.title
+}
+
+func (c *CreateRequest) Content() string {
+	return c.content
+}
+
+func NewCreateRequest(title string, content string) *CreateRequest {
+	return &CreateRequest{title: title, content: content}
 }
