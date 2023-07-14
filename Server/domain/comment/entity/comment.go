63e7e6083baa7ea4c61ab141edfc303f56dcfad3
yoochanhong <chanhongy6@naver.com>
2023-07-14 09:02:28 +0900

    Comment entity 캡슐화

--- Server/domain/comment/entity/comment.go
+++ Server/domain/comment/entity/comment.go
@@ -1,16 +1,32 @@
 package entity
 
 type Comment struct {
-	Id        int    `json:"id"`
-	PostId    int    `gorm:"column:postId"`
-	WriteTime string `gorm:"column:writeTime"` //db에 있는 컬럼 이름이랑 강제 매칭
-	Comment   string `json:"comment"`
+	id        int    `gorm:"column:id"`
+	postId    int    `gorm:"column:postId"`
+	writeTime string `gorm:"column:writeTime"` //db에 있는 컬럼 이름이랑 강제 매칭
+	comment   string `gorm:"column:comment"`
 }
 
 func NewComment(postId int, writeTime, comment string) *Comment {
 	return &Comment{
-		PostId:    postId,
-		WriteTime: writeTime,
-		Comment:   comment,
+		postId:    postId,
+		writeTime: writeTime,
+		comment:   comment,
 	}
+}
+
+func (c Comment) getId() int {
+	return c.id
+}
+
+func (c Comment) getPostId() int {
+	return c.postId
+}
+
+func (c Comment) getWriteTime() string {
+	return c.writeTime
+}
+
+func (c Comment) getComment() string {
+	return c.comment
 }
