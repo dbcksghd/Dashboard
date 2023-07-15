cff18482718f5c43bbcd36a3d85ad837f3d586c3
yoochanhong <chanhongy6@naver.com>
2023-07-15 23:19:49 +0900

    json 명시

--- Server/domain/comment/entity/comment.go
+++ Server/domain/comment/entity/comment.go
@@ -1,8 +1,8 @@
 package entity
 
 type Comment struct {
-	Id        int    `gorm:"column:id"`
-	PostId    int    `gorm:"column:postId"`
-	WriteTime string `gorm:"column:writeTime"` //db에 있는 컬럼 이름이랑 강제 매칭
-	Comment   string `gorm:"column:comment"`
+	Id        int    `json:"id"`
+	PostId    int    `json:"postId"`
+	WriteTime string `json:"writeTime"`
+	Comment   string `json:"comment"`
 }
