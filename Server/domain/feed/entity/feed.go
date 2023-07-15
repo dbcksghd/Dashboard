ccea46df30a3c66968a2810e57b3278a3b5cc327
yoochanhong <chanhongy6@naver.com>
2023-07-15 21:59:34 +0900

    field public 처리

--- Server/domain/feed/entity/feed.go
+++ Server/domain/feed/entity/feed.go
@@ -1,34 +1,7 @@
 package entity
 
 type Feed struct {
-	id      int    `gorm:"column:id"`
-	title   string `gorm:"column:title"`
-	content string `gorm:"column:content"`
-}
-
-func NewFeed(title, content string) *Feed {
-	return &Feed{
-		title:   title,
-		content: content,
-	}
-}
-
-func UpdateFeed(id int, title, content string) *Feed {
-	return &Feed{
-		id:      id,
-		title:   title,
-		content: content,
-	}
-}
-
-func (f *Feed) Id() int {
-	return f.id
-}
-
-func (f *Feed) Title() string {
-	return f.title
-}
-
-func (f *Feed) Content() string {
-	return f.content
+	Id      int    `gorm:"column:id"`
+	Title   string `gorm:"column:title"`
+	Content string `gorm:"column:content"`
 }
