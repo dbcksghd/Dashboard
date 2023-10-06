6d7b44d21cdef3f73d50b94bdc307af966198cb5
yoochanhong <chanhongy6@naver.com>
2023-10-07 00:51:39 +0900

    의존성 주입

---
+++ network_module/example/lib/di/di.dart
@@ -0,0 +1,19 @@
+import 'package:network_module/network_module.dart';
+import 'package:network_module_example/data/data_source.dart';
+import 'package:network_module_example/data/repository_impl.dart';
+import 'package:network_module_example/domain/repository.dart';
+import 'package:network_module_example/domain/use_case.dart';
+import 'package:network_module_example/presentation/view_model.dart';
+import 'package:provider/provider.dart';
+
+List<ChangeNotifierProvider> di() {
+  DataSource dataSource = DataSource(
+      executer:
+          NetworkExecuter(baseUrl: "https://jsonplaceholder.typicode.com"));
+  Repository repository = RepositoryImpl(dataSource: dataSource);
+  UseCase useCase = UseCase(repository: repository);
+  return [
+    ChangeNotifierProvider<ViewModel>(
+        create: (context) => ViewModel(useCase: useCase)),
+  ];
+}
