a9b469df1fa77bf5a642509b994a581d3b9dd456
yoochanhong <chanhongy6@naver.com>
2023-10-08 20:04:43 +0900

    path 이름 수정

--- network_module/lib/src/dio_request_options.dart
+++ network_module/lib/src/dio_request_options.dart
@@ -1,14 +1,14 @@
 import 'package:network_module/src/http_method.dart';
 
 class DioRequestOptions {
-  String? path;
+  String baseUrl;
   HTTPMethod httpMethod;
   Map<String, dynamic>? queryParam, body;
   Map<String, String>? headers;
 
   DioRequestOptions(
-      {required this.httpMethod,
-      this.path,
+      {required this.baseUrl,
+      required this.httpMethod,
       this.body,
       this.queryParam,
       this.headers});
