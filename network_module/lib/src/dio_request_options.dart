6913a635e36b5f4bbc5fa26434e6d533a7b87cfa
yoochanhong <chanhongy6@naver.com>
2023-10-08 19:48:36 +0900

    header타입 수정, onReceiveProgress 제거

--- network_module/lib/src/dio_request_options.dart
+++ network_module/lib/src/dio_request_options.dart
@@ -3,14 +3,13 @@ import 'package:network_module/src/http_method.dart';
 class DioRequestOptions {
   String? path;
   HTTPMethod httpMethod;
-  void Function(int, int)? onReceiveProgress;
-  Map<String, dynamic>? queryParam, headers, body;
+  Map<String, dynamic>? queryParam, body;
+  Map<String, String>? headers;
 
   DioRequestOptions(
       {required this.httpMethod,
       this.path,
       this.body,
-      this.onReceiveProgress,
       this.queryParam,
       this.headers});
 }
