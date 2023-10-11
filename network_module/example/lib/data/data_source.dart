cf068d790a5f16abc7ab55c3c86c103c849b051e
yoochanhong <chanhongy6@naver.com>
2023-10-11 10:05:53 +0900

    Result 클래스 사용

--- network_module/example/lib/data/data_source.dart
+++ network_module/example/lib/data/data_source.dart
@@ -7,11 +7,17 @@ class DataSource {
 
   DataSource({required HttpHelper httpHelper}) : _httpHelper = httpHelper;
 
-  Future<TestEntity> getTest() async {
+  Future<Result<TestEntity, Exception>> getTest() async {
     final res = await _httpHelper.network<Test>(
-        options:
-            DioRequestOptions(baseUrl: "https://jsonplaceholder.typicode.com/todos/1", httpMethod: HTTPMethod.get),
+        options: DioRequestOptions(
+            baseUrl: "https://jsonplaceholder.typicode.com/todos/1",
+            httpMethod: HTTPMethod.get),
         responseType: Test());
-    return res.toEntity();
+    switch (res) {
+      case Success(value: final value):
+        return Success(value: value.toEntity());
+      case Failure(exception: final e):
+        return Failure(exception: e);
+    }
   }
 }
