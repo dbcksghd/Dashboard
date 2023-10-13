69aae6395882655cdb754e41e18dd92ad10b4810
yoochanhong <chanhongy6@naver.com>
2023-10-13 21:57:06 +0900

    변경된 경로 재적용

--- network_module/example/lib/data/repository_impl.dart
+++ network_module/example/lib/data/repository_impl.dart
@@ -1,13 +1,15 @@
 import 'package:network_module/network_module.dart';
-import 'package:network_module_example/data/data_source.dart';
+import 'package:network_module_example/data/remote/remote_data_source.dart';
 import 'package:network_module_example/domain/entity.dart';
 import 'package:network_module_example/domain/repository.dart';
 
 class RepositoryImpl implements Repository {
-  final DataSource _dataSource;
+  final RemoteDataSource _dataSource;
 
-  RepositoryImpl({required DataSource dataSource}) : _dataSource = dataSource;
+  RepositoryImpl({required RemoteDataSource dataSource})
+      : _dataSource = dataSource;
 
   @override
-  Future<Result<TestEntity, Exception>> getTest() async => _dataSource.getTest();
+  Future<Result<TestEntity, Exception>> getTest() async =>
+      _dataSource.getTest();
 }
--- network_module/example/lib/di/di.dart
+++ network_module/example/lib/di/di.dart
@@ -1,5 +1,5 @@
 import 'package:network_module/network_module.dart';
-import 'package:network_module_example/data/data_source.dart';
+import 'package:network_module_example/data/remote/remote_data_source.dart';
 import 'package:network_module_example/data/repository_impl.dart';
 import 'package:network_module_example/domain/repository.dart';
 import 'package:network_module_example/domain/use_case.dart';
@@ -7,7 +7,7 @@ import 'package:network_module_example/presentation/view_model.dart';
 import 'package:provider/provider.dart';
 
 List<ChangeNotifierProvider> di() {
-  DataSource dataSource = DataSource(httpHelper: HttpHelper());
+  RemoteDataSource dataSource = RemoteDataSource(httpHelper: HttpHelper());
   Repository repository = RepositoryImpl(dataSource: dataSource);
   UseCase useCase = UseCase(repository: repository);
   return [
