c30f44deea30f8712babbdbd94a714973a6a1b3e
yoochanhong <chanhongy6@naver.com>
2023-10-14 16:58:45 +0900

    data_source 방식 변경

--- network_module/example/lib/data/remote/remote_data_source.dart
+++ network_module/example/lib/data/remote/remote_data_source.dart
@@ -1,18 +1,17 @@
 import 'package:network_module/network_module.dart';
+import 'package:network_module_example/core/networking/interface/networking.dart';
+import 'package:network_module_example/data/remote/todo_endpoint.dart';
 import 'package:network_module_example/data/response_dto.dart';
 import 'package:network_module_example/domain/entity.dart';
 
 class RemoteDataSource {
-  final HttpHelper _httpHelper;
+  final Networking networking;
 
-  RemoteDataSource({required HttpHelper httpHelper}) : _httpHelper = httpHelper;
+  RemoteDataSource({required this.networking});
 
   Future<Result<TestEntity, Exception>> getTest() async {
-    final res = await _httpHelper.network<Test>(
-        options: DioRequestOptions(
-            baseUrl: "https://jsonplaceholder.typicode.com/todos/1",
-            httpMethod: HTTPMethod.get),
-        responseType: Test());
+    final res = await networking.request<Test>(
+        options: TodoEndpoint.todos, responseType: Test());
     return switch (res) {
       Success(value: final value) => Success(value: value.toEntity()),
       Failure(exception: final e) => Failure(exception: e),
