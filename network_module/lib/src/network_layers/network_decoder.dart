fc6cfed284b299ab709ba68c620f390b19f1e51d
yoochanhong <chanhongy6@naver.com>
2023-10-14 17:42:58 +0900

    반환타입을 K로 지정

--- network_module/lib/src/network_layers/network_decoder.dart
+++ network_module/lib/src/network_layers/network_decoder.dart
@@ -4,16 +4,15 @@ import 'package:network_module/src/base_network_dto.dart';
 class NetworkDecoder {
   static var decodeHelper = NetworkDecoder();
 
-  T decode<T extends BaseResponseDTO>(
-      {required Response<dynamic> response,
-      required BaseResponseDTO responseType}) {
+  K decode<T extends BaseResponseDTO, K>(
+      {required Response<dynamic> response, required T responseType}) {
     try {
       if (response.data is List) {
         var list = response.data as List;
         return List<T>.from(
-            list.map((item) => responseType.fromJson(item)).toList()) as T;
+            list.map((item) => responseType.fromJson(item)).toList()) as K;
       } else {
-        return responseType.fromJson(response.data) as T;
+        return responseType.fromJson(response.data) as K;
       }
     } on TypeError {
       rethrow;
