232ee4cfcbd8d5dfde71f019600dfee46702edee
yoochanhong <chanhongy6@naver.com>
2023-10-14 20:58:20 +0900

    엔드포인트 String 타입으로 변경

--- network_module/example/lib/core/networking/interface/endpoint/test_endpoint.dart
+++ network_module/example/lib/core/networking/interface/endpoint/test_endpoint.dart
@@ -2,12 +2,12 @@ import 'package:network_module/network_module.dart';
 import 'package:network_module_example/core/networking/interface/endpoint/test_rest_api_domain.dart';
 
 mixin TestEndpoint implements DioRequestOptions {
-  TestRestAPIDomain get domain;
+  String get domain;
 
   Map<int, Error> get errorMap;
 
   @override
-  String get baseUrl => "https://jsonplaceholder.typicode.com/${domain.name}";
+  String get baseUrl => "https://jsonplaceholder.typicode.com/${domain}";
 
   Map<String, String> get headers => {'Content-Type': 'application/json'};
 }
--- network_module/example/lib/data/remote/todo_endpoint.dart
+++ network_module/example/lib/data/remote/todo_endpoint.dart
@@ -1,6 +1,5 @@
 import 'package:network_module/network_module.dart';
 import 'package:network_module_example/core/networking/interface/endpoint/test_endpoint.dart';
-import 'package:network_module_example/core/networking/interface/endpoint/test_rest_api_domain.dart';
 
 enum TodoEndpoint with TestEndpoint {
   todos();
@@ -10,7 +9,7 @@ enum TodoEndpoint with TestEndpoint {
   const TodoEndpoint({this.baseRequestDTO});
 
   @override
-  TestRestAPIDomain get domain => TestRestAPIDomain.todos;
+  String get domain => switch (this) { todos => "todos" };
 
   @override
   HTTPMethod get httpMethod => switch (this) { todos => HTTPMethod.get };
