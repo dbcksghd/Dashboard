509e8522ce80a24871b8ffbae4b01009c9fa3ce1
yoochanhong <chanhongy6@naver.com>
2023-10-16 23:15:17 +0900

    mixin => 추상 클래스

--- network_module/example/lib/core/networking/interface/endpoint/test_endpoint.dart
+++ network_module/example/lib/core/networking/interface/endpoint/test_endpoint.dart
@@ -1,7 +1,7 @@
 import 'package:network_module/network_module.dart';
 import 'package:network_module_example/core/networking/interface/endpoint/test_rest_api_domain.dart';
 
-mixin TestEndpoint implements DioRequestOptions {
+abstract class TestEndpoint extends DioRequestOptions {
   String get domain;
 
   Map<int, Exception> get errorMap;
