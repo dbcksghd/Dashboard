05621775fc238cce5cc891a14d5aa34b0b2897a9
yoochanhong <chanhongy6@naver.com>
2023-10-16 23:44:58 +0900

    DashboardEndpoint 추상클래스 생성

---
+++ Client/lib/core/network/interface/endpoint/dashboard_endpoint.dart
@@ -0,0 +1,15 @@
+import 'package:client/core/network/interface/endpoint/dasboard_rest_api_domain.dart';
+import 'package:client/core/network/interface/interceptor/jwt/jwt_authorizable.dart';
+import 'package:network_module/network_module.dart';
+
+abstract class DashboardEndpoint implements DioRequestOptions, JwtAuthorizable {
+  DashboardRestAPIDomain get domain;
+
+  Map<int, Error> get errorMap;
+
+  @override
+  String get baseUrl => "http://localhost:8080/${domain.name}";
+
+  @override
+  Map<String, String> get headers => {'Content-Type': 'application/json'};
+}
