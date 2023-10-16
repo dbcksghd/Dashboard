3faf476e9222ac0c9bd2b9e0ee13a5f3abaa9eb5
yoochanhong <chanhongy6@naver.com>
2023-10-16 23:46:23 +0900

    networking interface impl

---
+++ Client/lib/core/network/source/network_impl.dart
@@ -0,0 +1,15 @@
+import 'package:client/core/network/interface/network.dart';
+import 'package:network_module/network_module.dart';
+
+class NetworkingImpl implements Networking {
+  final HttpHelper _httpHelper;
+
+  NetworkingImpl({required HttpHelper httpHelper}) : _httpHelper = httpHelper;
+
+  @override
+  Future<Result<K, Exception>> request<T extends BaseResponseDTO, K>(
+          {required DioRequestOptions options,
+          required T responseType}) async =>
+      await _httpHelper.network<T, K>(
+          options: options, responseType: responseType);
+}
