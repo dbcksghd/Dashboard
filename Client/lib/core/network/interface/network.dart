1b165c399e2217201d0acece58b56fa92c84fe57
yoochanhong <chanhongy6@naver.com>
2023-10-16 23:46:14 +0900

    networking interface

---
+++ Client/lib/core/network/interface/network.dart
@@ -0,0 +1,6 @@
+import 'package:network_module/network_module.dart';
+
+abstract class Networking {
+  Future<Result<K, Exception>> request<T extends BaseResponseDTO, K>(
+      {required DioRequestOptions options, required T responseType});
+}
