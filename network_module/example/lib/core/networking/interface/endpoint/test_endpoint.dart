53290b6322e5f03661b40b4048cd28c3dfe29de0
yoochanhong <chanhongy6@naver.com>
2023-10-17 22:17:31 +0900

    domain => path

--- network_module/example/lib/core/networking/interface/endpoint/test_endpoint.dart
+++ network_module/example/lib/core/networking/interface/endpoint/test_endpoint.dart
@@ -2,12 +2,12 @@ import 'package:network_module/network_module.dart';
 import 'package:network_module_example/core/networking/interface/endpoint/test_rest_api_domain.dart';
 
 abstract class TestEndpoint extends DioRequestOptions {
-  String get domain;
+  String get path;
 
   Map<int, Exception> get errorMap;
 
   @override
-  String get baseUrl => "https://jsonplaceholder.typicode.com/${domain}";
+  String get baseUrl => "https://jsonplaceholder.typicode.com/${path}";
 
   @override
   Map<String, String> get headers => {'Content-Type': 'application/json'};
