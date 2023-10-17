ff8de7ccbad4ce475aa5074429f72a3e207330e5
yoochanhong <chanhongy6@naver.com>
2023-10-17 23:34:57 +0900

    jwt store 인터페이스

---
+++ Client/lib/core/jwt_store/interface/jwt_store.dart
@@ -0,0 +1,10 @@
+import 'package:client/core/jwt_store/interface/jwt_store_properties.dart';
+
+abstract interface class JwtStore {
+  Future<void> save(
+      {required JwtStoreProperties properties, required String token});
+
+  Future<String?> load({required JwtStoreProperties properties});
+
+  Future<void> delete(JwtStoreProperties properties);
+}
