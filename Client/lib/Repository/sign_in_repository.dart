a7c2c8910a0804d08de2ebfdf793fd957526a5f5
yoochanhong <chanhongy6@naver.com>
2023-10-17 23:26:23 +0900

    로그인 관련 레거시 코드 삭제

--- Client/lib/Repository/sign_in_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:client/DataSource/sign_in_data_source.dart';
-import 'package:client/Model/sign_in_response.dart';
-
-class SignInRepository {
-  final SignInDataSource _dataSource = SignInDataSource();
-
-  Future<SignInResponse> signIn(String id, password) async =>
-      await _dataSource.signIn(id, password);
-}
