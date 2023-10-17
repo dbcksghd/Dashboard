b4a9379fbd6dd15e3503616e848be25969c002dc
yoochanhong <chanhongy6@naver.com>
2023-10-17 14:52:17 +0900

    Response 클래스 export

--- network_module/lib/network_module.dart
+++ network_module/lib/network_module.dart
@@ -1,5 +1,7 @@
 library network_module;
 
+export 'package:dio/dio.dart' show Response;
+
 export 'src/base_network_dto.dart';
 export 'src/dio_request_options.dart';
 export 'src/http_method.dart';
