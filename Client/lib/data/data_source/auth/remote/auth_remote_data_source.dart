f1fd082cc3d3b37c820237ed487e59e820be2a6b
yoochanhong <chanhongy6@naver.com>
2023-10-17 23:20:26 +0900

    auth_remote_data_source 추가

---
+++ Client/lib/data/data_source/auth/remote/auth_remote_data_source.dart
@@ -0,0 +1,25 @@
+import 'package:client/core/network/interface/network.dart';
+import 'package:client/data/data_source/auth/remote/auth_endpoint.dart';
+import 'package:client/data/dto/request/auth/sign_in_request_dto.dart';
+import 'package:client/data/dto/response/auth/sign_in_response_dto.dart';
+import 'package:client/domain/entity/auth/sign_in_entity.dart';
+import 'package:network_module/network_module.dart';
+
+class AuthRemoteDataSource {
+  final Networking _networking;
+
+  AuthRemoteDataSource({required Networking networking})
+      : _networking = networking;
+
+  Future<Result<SignInEntity, Exception>> signIn(
+      SignInRequestDTO signInRequestDTO) async {
+    final response =
+        await _networking.request<SignInResponseDTO, SignInResponseDTO>(
+            options: AuthEndpoint.signIn(signInRequestDTO: signInRequestDTO),
+            responseType: SignInResponseDTO());
+    return switch (response) {
+      Success(value: final value) => Success(value: value.toEntity()),
+      Failure(exception: final e) => Failure(exception: e),
+    };
+  }
+}
