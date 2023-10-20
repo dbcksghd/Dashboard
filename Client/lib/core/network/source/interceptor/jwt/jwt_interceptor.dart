3a4f3739be6cd31a7ee18c80d60149f63a30d1de
yoochanhong <chanhongy6@naver.com>
2023-10-20 16:15:20 +0900

    토큰 담기 && 토큰 저장 인터셉터 추가

---
+++ Client/lib/core/network/source/interceptor/jwt/jwt_interceptor.dart
@@ -0,0 +1,36 @@
+import 'package:client/core/jwt_store/interface/jwt_store.dart';
+import 'package:client/core/jwt_store/interface/jwt_store_properties.dart';
+import 'package:client/core/network/interface/endpoint/dashboard_request_options.dart';
+import 'package:network_module/network_module.dart';
+
+class JWTInterceptor extends Interceptor<DashboardRequestOptions> {
+  final JwtStore _jwtStore;
+
+  JWTInterceptor({required JwtStore jwtStore}) : _jwtStore = jwtStore;
+
+  @override
+  void onRequest(DashboardRequestOptions options) async {
+    if (JwtStoreProperties.accessToken.name == options.jwtTokenType.name) {
+      String accessToken =
+          await _jwtStore.load(properties: JwtStoreProperties.accessToken);
+      if (accessToken.isNotEmpty) {
+        options.headers!['Authorization'] = "Bearer $accessToken";
+      }
+    }
+  }
+
+  @override
+  void onResponse(Response response) async {
+    if (response.data['access_token'] != null) {
+      await _jwtStore.save(
+          properties: JwtStoreProperties.accessToken,
+          token: response.data['access_token']);
+      await _jwtStore.save(
+          properties: JwtStoreProperties.refreshToken,
+          token: response.data['refresh_token']);
+    }
+  }
+
+  @override
+  void onError(Object error) {}
+}
