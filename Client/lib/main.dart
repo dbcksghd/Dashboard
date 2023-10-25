ea8f3725208c93d87e7f4944eedaf9214dcd74f3
yoochanhong <chanhongy6@naver.com>
2023-10-25 18:29:36 +0900

    기능 롤백

--- Client/lib/main.dart
+++ Client/lib/main.dart
@@ -1,28 +1,21 @@
-import 'package:client/View/Screen/feed_page.dart';
-import 'package:client/View/Screen/sign_in_page.dart';
-import 'package:client/ViewModel/comment_view_model.dart';
-import 'package:client/ViewModel/feed_view_model.dart';
-import 'package:client/ViewModel/sign_in_view_model.dart';
+import 'package:client/di/di.dart';
+import 'package:client/presentation/feed_page/feed_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MultiProvider(
+      providers: di(),
+      child: const MyApp(),
+    ));
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => FeedViewModel()),
-        ChangeNotifierProvider(create: (context) => CommentViewModel()),
-        ChangeNotifierProvider(create: (context) => SignInViewModel()),
-      ],
-      child: const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: SignInPage(),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FeedPage(),
     );
   }
 }
