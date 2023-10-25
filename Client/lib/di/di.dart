9e03f969a9b64ca1fd3b6f5a669e0f554fed9dea
yoochanhong <chanhongy6@naver.com>
2023-10-25 23:02:08 +0900

    JwtStore 주입

--- Client/lib/di/di.dart
+++ Client/lib/di/di.dart
@@ -1,15 +1,22 @@
+import 'package:client/core/jwt_store/interface/jwt_store.dart';
+import 'package:client/core/jwt_store/source/jwt_store_impl.dart';
 import 'package:client/core/network/interface/network.dart';
+import 'package:client/core/network/source/interceptor/jwt/jwt_interceptor.dart';
 import 'package:client/core/network/source/network_impl.dart';
 import 'package:client/data/data_source/feed/remote/remote_feed_data_source.dart';
 import 'package:client/data/repository/feed/feed_repository_impl.dart';
 import 'package:client/domain/repository/feed_repository.dart';
 import 'package:client/domain/use_case/feed/get_all_feeds_use_case.dart';
 import 'package:client/presentation/feed_page/feed_page_view_model.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:network_module/network_module.dart';
 import 'package:provider/provider.dart';
 
 List<ChangeNotifierProvider> di() {
-  HttpHelper httpHelper = HttpHelper();
+  FlutterSecureStorage storage = const FlutterSecureStorage();
+  JwtStore jwtStore = JwtStoreImpl(storage: storage);
+  HttpHelper httpHelper = HttpHelper()
+    ..interceptor.add(JWTInterceptor(jwtStore: jwtStore));
   Networking networking = NetworkingImpl(httpHelper: httpHelper);
   RemoteFeedDataSource remoteFeedDataSource =
       RemoteFeedDataSource(networking: networking);
