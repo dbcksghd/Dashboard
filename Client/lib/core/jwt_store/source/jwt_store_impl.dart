beca11041ca661b45f21c1ef466d880c5921c5cd
yoochanhong <chanhongy6@naver.com>
2023-10-25 23:00:46 +0900

    JwtStore 구현

--- Client/lib/core/jwt_store/source/jwt_store_impl.dart
+++ Client/lib/core/jwt_store/source/jwt_store_impl.dart
@@ -1,16 +1,25 @@
+import 'package:client/core/jwt_store/interface/jwt_store.dart';
 import 'package:client/core/jwt_store/interface/jwt_store_properties.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-class JwtStoreImpl {
+class JwtStoreImpl implements JwtStore {
   final FlutterSecureStorage _storage;
 
   JwtStoreImpl({required FlutterSecureStorage storage}) : _storage = storage;
 
+  @override
   Future<void> save(
           {required JwtStoreProperties properties,
           required String token}) async =>
       await _storage.write(key: properties.name, value: token);
 
-  Future<String?> load({required JwtStoreProperties properties}) async =>
-      await _storage.read(key: properties.name);
+  @override
+  Future<String> load({required JwtStoreProperties properties}) async =>
+      await _storage.read(key: properties.name) ?? "";
+
+  @override
+  Future<void> delete(JwtStoreProperties properties) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
 }
