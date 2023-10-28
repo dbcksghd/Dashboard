2ef003fb9d48b2c3684c557725d836acdc4018a0
yoochanhong <chanhongy6@naver.com>
2023-10-28 15:46:25 +0900

    Dashboard옵션으로 매개변수 변경

--- Client/lib/core/network/interface/network.dart
+++ Client/lib/core/network/interface/network.dart
@@ -1,6 +1,7 @@
+import 'package:client/core/network/interface/endpoint/dashboard_request_options.dart';
 import 'package:network_module/network_module.dart';
 
 abstract class Networking {
   Future<Result<K, Exception>> request<T extends BaseResponseDTO, K>(
-      {required DioRequestOptions options, required T responseType});
+      {required DashboardRequestOptions options, required T responseType});
 }
