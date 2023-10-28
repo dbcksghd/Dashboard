f84b8404ab9f2684bdf5d48296008486dda7187d
yoochanhong <chanhongy6@naver.com>
2023-10-28 20:21:24 +0900

    생성자로 필드 받아오기

--- Client/lib/data/dto/request/auth/sign_in_request_dto.dart
+++ Client/lib/data/dto/request/auth/sign_in_request_dto.dart
@@ -1,10 +1,10 @@
 import 'package:network_module/network_module.dart';
 
 final class SignInRequestDTO extends BaseRequestDTO {
-  late String id;
-  late String password;
+  final String id;
+  final String password;
 
-  SignInRequestDTO();
+  SignInRequestDTO({required this.id, required this.password});
 
   @override
   Map<String, dynamic> toJson() => {'id': id, 'password': password};
