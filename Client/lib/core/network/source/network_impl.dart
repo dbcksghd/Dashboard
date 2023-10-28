ba970ab5049785c56bd561f7d921129fde8641ca
yoochanhong <chanhongy6@naver.com>
2023-10-28 15:48:13 +0900

    http helper 역할을 고유 네트워킹 구현체로 변경

--- Client/lib/core/network/source/network_impl.dart
+++ Client/lib/core/network/source/network_impl.dart
@@ -1,15 +1,32 @@
+import 'package:client/core/network/interface/endpoint/dashboard_request_options.dart';
 import 'package:client/core/network/interface/network.dart';
 import 'package:network_module/network_module.dart';
 
 class NetworkingImpl implements Networking {
-  final HttpHelper _httpHelper;
-
-  NetworkingImpl({required HttpHelper httpHelper}) : _httpHelper = httpHelper;
+  List<Interceptor> interceptor = List.empty(growable: true);
 
   @override
   Future<Result<K, Exception>> request<T extends BaseResponseDTO, K>(
-          {required DioRequestOptions options,
-          required T responseType}) async =>
-      await _httpHelper.network<T, K>(
-          options: options, responseType: responseType);
+      {required DashboardRequestOptions options,
+      required T responseType}) async {
+    try {
+      for (var e in interceptor) {
+        await e.onRequest(options);
+      }
+      final response = await Network.network.request(options: options);
+      for (var e in interceptor) {
+        e.onResponse(response);
+      }
+      final decodeData = NetworkDecoder.decodeHelper.decode<T, K>(
+        response: response,
+        responseType: responseType,
+      );
+      return Success(value: decodeData);
+    } catch (error) {
+      for (var e in interceptor) {
+        e.onError(error);
+      }
+      return Failure(exception: Exception(error));
+    }
+  }
 }
