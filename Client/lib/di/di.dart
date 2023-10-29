22507486195d8dc564f4c46bab4199ac237eb758
yoochanhong <chanhongy6@naver.com>
2023-10-29 12:10:50 +0900

    FeedPageViewModel usecase 의존성 주입

--- Client/lib/di/di.dart
+++ Client/lib/di/di.dart
@@ -6,7 +6,10 @@ import 'package:client/core/network/source/network_impl.dart';
 import 'package:client/data/data_source/feed/remote/remote_feed_data_source.dart';
 import 'package:client/data/repository/feed/feed_repository_impl.dart';
 import 'package:client/domain/repository/feed_repository.dart';
+import 'package:client/domain/use_case/feed/create_feed_use_case.dart';
+import 'package:client/domain/use_case/feed/delete_feed_use_case.dart';
 import 'package:client/domain/use_case/feed/get_all_feeds_use_case.dart';
+import 'package:client/domain/use_case/feed/update_feed_use_case.dart';
 import 'package:client/presentation/feed_page/feed_page_view_model.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:provider/provider.dart';
@@ -22,9 +25,18 @@ List<ChangeNotifierProvider> di() {
       FeedRepositoryImpl(remoteFeedDataSource: remoteFeedDataSource);
   GetAllFeedsUseCase getAllFeedsUseCase =
       GetAllFeedsUseCase(feedRepository: feedRepository);
+  CreateFeedUseCase createFeedUseCase =
+      CreateFeedUseCase(feedRepository: feedRepository);
+  UpdateFeedUseCase updateFeedUseCase =
+      UpdateFeedUseCase(feedRepository: feedRepository);
+  DeleteFeedUseCase deleteFeedUseCase =
+      DeleteFeedUseCase(feedRepository: feedRepository);
   return [
     ChangeNotifierProvider<FeedPageViewModel>(
-        create: (context) =>
-            FeedPageViewModel(getAllFeedsUseCase: getAllFeedsUseCase)),
+        create: (context) => FeedPageViewModel(
+            getAllFeedsUseCase: getAllFeedsUseCase,
+            createFeedUseCase: createFeedUseCase,
+            updateFeedUseCase: updateFeedUseCase,
+            deleteFeedUseCase: deleteFeedUseCase)),
   ];
 }
