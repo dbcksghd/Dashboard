c3376f0deb4c983b193134b0f222bdefdc19fa66
yoochanhong <chanhongy6@naver.com>
2023-10-29 16:31:24 +0900

    댓글 뷰모델 의존성 주입

--- Client/lib/di/di.dart
+++ Client/lib/di/di.dart
@@ -3,13 +3,19 @@ import 'package:client/core/jwt_store/source/jwt_store_impl.dart';
 import 'package:client/core/network/interface/network.dart';
 import 'package:client/core/network/source/interceptor/jwt/jwt_interceptor.dart';
 import 'package:client/core/network/source/network_impl.dart';
+import 'package:client/data/data_source/comment/remote/remote_comment_data_source.dart';
 import 'package:client/data/data_source/feed/remote/remote_feed_data_source.dart';
+import 'package:client/data/repository/comment/comment_repository_impl.dart';
 import 'package:client/data/repository/feed/feed_repository_impl.dart';
+import 'package:client/domain/repository/comment_repository.dart';
 import 'package:client/domain/repository/feed_repository.dart';
+import 'package:client/domain/use_case/comment/create_comment_use_case.dart';
+import 'package:client/domain/use_case/comment/get_all_comments_use_case.dart';
 import 'package:client/domain/use_case/feed/create_feed_use_case.dart';
 import 'package:client/domain/use_case/feed/delete_feed_use_case.dart';
 import 'package:client/domain/use_case/feed/get_all_feeds_use_case.dart';
 import 'package:client/domain/use_case/feed/update_feed_use_case.dart';
+import 'package:client/presentation/content_page/content_page_view_model.dart';
 import 'package:client/presentation/feed_page/feed_page_view_model.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:provider/provider.dart';
@@ -21,8 +27,14 @@ List<ChangeNotifierProvider> di() {
     ..interceptor.add(JWTInterceptor(jwtStore: jwtStore));
   RemoteFeedDataSource remoteFeedDataSource =
       RemoteFeedDataSource(networking: networking);
+  RemoteCommentDataSource remoteCommentDataSource =
+      RemoteCommentDataSource(networking: networking);
+
   FeedRepository feedRepository =
       FeedRepositoryImpl(remoteFeedDataSource: remoteFeedDataSource);
+  CommentRepository commentRepository =
+      CommentRepositoryImpl(remoteCommentDataSource: remoteCommentDataSource);
+
   GetAllFeedsUseCase getAllFeedsUseCase =
       GetAllFeedsUseCase(feedRepository: feedRepository);
   CreateFeedUseCase createFeedUseCase =
@@ -31,6 +43,11 @@ List<ChangeNotifierProvider> di() {
       UpdateFeedUseCase(feedRepository: feedRepository);
   DeleteFeedUseCase deleteFeedUseCase =
       DeleteFeedUseCase(feedRepository: feedRepository);
+  CreateCommentUseCase createCommentUseCase =
+      CreateCommentUseCase(commentRepository: commentRepository);
+  GetAllCommentsUseCase getAllCommentsUseCase =
+      GetAllCommentsUseCase(commentRepository: commentRepository);
+
   return [
     ChangeNotifierProvider<FeedPageViewModel>(
         create: (context) => FeedPageViewModel(
@@ -38,5 +55,10 @@ List<ChangeNotifierProvider> di() {
             createFeedUseCase: createFeedUseCase,
             updateFeedUseCase: updateFeedUseCase,
             deleteFeedUseCase: deleteFeedUseCase)),
+    ChangeNotifierProvider<ContentPageViewModel>(
+      create: (context) => ContentPageViewModel(
+          getAllCommentsUseCase: getAllCommentsUseCase,
+          createCommentUseCase: createCommentUseCase),
+    ),
   ];
 }
