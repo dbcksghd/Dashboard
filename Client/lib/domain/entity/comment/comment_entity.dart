aec2324a65e4be4c03f6f0c7757f6a776a6afe0c
yoochanhong <chanhongy6@naver.com>
2023-10-29 15:56:50 +0900

    comment => content

--- Client/lib/domain/entity/comment/comment_entity.dart
+++ Client/lib/domain/entity/comment/comment_entity.dart
@@ -1,11 +1,11 @@
 class CommentEntity {
   final int id, feedId;
-  final String writeTime, comment;
+  final String writeTime, content;
 
   CommentEntity({
     required this.id,
     required this.feedId,
     required this.writeTime,
-    required this.comment,
+    required this.content,
   });
 }
