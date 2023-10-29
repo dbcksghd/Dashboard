2c279914c17cbf7af6ba35bc6e77a5b25e44bcf0
yoochanhong <chanhongy6@naver.com>
2023-10-29 09:35:54 +0900

    피드 수정, 생성, 삭제 데이터소스 추가

--- Client/lib/data/data_source/feed/remote/remote_feed_data_source.dart
+++ Client/lib/data/data_source/feed/remote/remote_feed_data_source.dart
@@ -1,5 +1,7 @@
 import 'package:client/core/network/interface/network.dart';
 import 'package:client/data/data_source/feed/remote/feed_endpoint.dart';
+import 'package:client/data/dto/request/feed/create_feed_request_dto.dart';
+import 'package:client/data/dto/request/feed/update_feed_request_dto.dart';
 import 'package:client/data/dto/response/feed/get_all_feeds_response_dto.dart';
 import 'package:client/domain/entity/feed/feed_entity.dart';
 import 'package:network_module/network_module.dart';
@@ -20,5 +22,24 @@ class RemoteFeedDataSource {
         Success(value: value.map((e) => e.toEntity()).toList()),
       Failure(exception: final e) => Failure(exception: e),
     };
+  }
+
+  Future<Result<void, Exception>> createFeed(
+      {required CreateFeedRequestDTO createFeedRequestDTO}) async {
+    return await _networking.noResponseRequest(
+        options: FeedEndpoint.createFeed(
+            createFeedRequestDTO: createFeedRequestDTO));
+  }
+
+  Future<Result<void, Exception>> updateFeed(
+      {required UpdateFeedRequestDTO updateFeedRequestDTO}) async {
+    return await _networking.noResponseRequest(
+        options: FeedEndpoint.updateFeed(
+            updateFeedRequestDTO: updateFeedRequestDTO));
+  }
+
+  Future<Result<void, Exception>> deleteFeed({required int id}) async {
+    return await _networking.noResponseRequest(
+        options: FeedEndpoint.deleteFeed(id: id));
   }
 }
