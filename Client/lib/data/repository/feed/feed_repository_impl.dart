dff1904643021ef44d48f6d084e7db282925469d
yoochanhong <chanhongy6@naver.com>
2023-10-29 09:38:40 +0900

    피드 레포지토리 구현

--- Client/lib/data/repository/feed/feed_repository_impl.dart
+++ Client/lib/data/repository/feed/feed_repository_impl.dart
@@ -1,4 +1,6 @@
 import 'package:client/data/data_source/feed/remote/remote_feed_data_source.dart';
+import 'package:client/data/dto/request/feed/create_feed_request_dto.dart';
+import 'package:client/data/dto/request/feed/update_feed_request_dto.dart';
 import 'package:client/domain/entity/feed/feed_entity.dart';
 import 'package:client/domain/repository/feed_repository.dart';
 import 'package:network_module/network_module.dart';
@@ -10,6 +12,22 @@ class FeedRepositoryImpl implements FeedRepository {
       : _remoteFeedDataSource = remoteFeedDataSource;
 
   @override
-  Future<Result<List<FeedEntity>, Exception>> getAllFeeds() =>
-      _remoteFeedDataSource.getAllFeeds();
+  Future<Result<List<FeedEntity>, Exception>> getAllFeeds() async =>
+      await _remoteFeedDataSource.getAllFeeds();
+
+  @override
+  Future<Result<void, Exception>> createFeed(
+          {required CreateFeedRequestDTO createFeedRequestDTO}) async =>
+      await _remoteFeedDataSource.createFeed(
+          createFeedRequestDTO: createFeedRequestDTO);
+
+  @override
+  Future<Result<void, Exception>> deleteFeed({required int id}) async =>
+      await _remoteFeedDataSource.deleteFeed(id: id);
+
+  @override
+  Future<Result<void, Exception>> updateFeed(
+          {required UpdateFeedRequestDTO updateFeedRequestDTO}) async =>
+      _remoteFeedDataSource.updateFeed(
+          updateFeedRequestDTO: updateFeedRequestDTO);
 }
