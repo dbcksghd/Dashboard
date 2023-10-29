d0f56a779f90298378572bf845b847c6b320182f
yoochanhong <chanhongy6@naver.com>
2023-10-29 09:38:31 +0900

    피드 레포지토리 인터페이스 정의

--- Client/lib/domain/repository/feed_repository.dart
+++ Client/lib/domain/repository/feed_repository.dart
@@ -1,6 +1,16 @@
+import 'package:client/data/dto/request/feed/create_feed_request_dto.dart';
+import 'package:client/data/dto/request/feed/update_feed_request_dto.dart';
 import 'package:client/domain/entity/feed/feed_entity.dart';
 import 'package:network_module/network_module.dart';
 
 abstract interface class FeedRepository {
   Future<Result<List<FeedEntity>, Exception>> getAllFeeds();
+
+  Future<Result<void, Exception>> createFeed(
+      {required CreateFeedRequestDTO createFeedRequestDTO});
+
+  Future<Result<void, Exception>> updateFeed(
+      {required UpdateFeedRequestDTO updateFeedRequestDTO});
+
+  Future<Result<void, Exception>> deleteFeed({required int id});
 }
