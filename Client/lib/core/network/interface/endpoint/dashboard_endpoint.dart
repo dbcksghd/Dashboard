a8c993b1a6bf7451c5e664fd5f886c413c3f1f88
yoochanhong <chanhongy6@naver.com>
2023-10-30 19:51:15 +0900

    Error => Exception으로 변경

--- Client/lib/core/network/interface/endpoint/dashboard_endpoint.dart
+++ Client/lib/core/network/interface/endpoint/dashboard_endpoint.dart
@@ -6,7 +6,7 @@ abstract class DashboardEndpoint extends DashboardRequestOptions {
 
   DashboardRestAPIDomain get domain;
 
-  Map<int, Error> get errorMap;
+  Map<int, Exception> get errorMap;
 
   @override
   String get baseUrl => "http://localhost:8080$path";
