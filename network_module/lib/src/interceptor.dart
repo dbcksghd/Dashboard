a93d9442b528368397e40f7ec628f2b247feeea2
yoochanhong <chanhongy6@naver.com>
2023-11-08 22:37:18 +0900

    클래스 단에서 구현하도록 변경

--- network_module/lib/src/interceptor.dart
+++ network_module/lib/src/interceptor.dart
@@ -1,9 +1,10 @@
 import 'package:network_module/network_module.dart';
 
 abstract class Interceptor<T extends DioRequestOptions> {
-  Future<void> onRequest(T endpoint);
+  Future<void> onRequest(T endpoint) async {}
 
-  Future<void> onResponse(T endpoint, Response<dynamic> response);
+  Future<void> onResponse(T endpoint, Response<dynamic> response) async {}
 
-  Future<void> onError(Object error);
+  Future<void> onError(Object error) async {}
+
 }
